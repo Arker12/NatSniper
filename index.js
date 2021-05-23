cff6094f2124a6b51c95522e4711c3a76c627993
Nats <natsishere@gmail.com>
2021-05-23 21:49:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const title = require('console-title');
 const notifier = require('node-notifier');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const config = require('./config.json');
+const webhookClient = new Discord.WebhookClient(config.webhookID, config.webhookToken);
 let token = config.token;
 let count = 0;
+
 
 bot.on("ready", () => {
     console.log(`Hey ${chalk.magenta(bot.user.tag)}, happy to see you !\nLets start the sniper :)`);
@@ -25,7 +28,7 @@ bot.on("ready", () => {
 					large_image : "natsniper",
 					large_text : "NatSniper" 
 				},
-			buttons : [{label : "Github Repo" , url : "https://github.com/Nats-uuu/NatSniper"}]
+			buttons : [{label : "Github Repo", url : "https://github.com/Nats-uuu/NatSniper"}]
 		}
 	})
 })
@@ -33,7 +36,7 @@ client.login({ clientId : "837842013088645160" });
 
       }
     catch(err) {
-        console.log(`I couldn't set your rich presence... \nMake an issue if there is any problem`)
+        console.log(`I couldn't set your rich presence... \nCheck the repo if there is any problem`)
     }
 });
 
@@ -45,7 +48,7 @@ bot.on("message", message => {
         let code;
         if (message.channel.type != 'dm' && message.channel.type != 'group') {
             // Nitro Sniper
-            if (message.content.includes("discord.gift") || message.content.includes("discordapp.com/gifts/") || message.content.includes("discord.com/gifts/") {
+            if (message.content.includes("discord.gift") || message.content.includes("discordapp.com/gifts/")) {
                 var start = new Date();
                 console.log(`[${chalk.bgMagenta("GIFT")}] - [${chalk.cyan(message.guild.name)}] - ${chalk.blue(message.author.tag)}: ${chalk.underline(message.content)}`);
                 if (message.content.includes("discord.gift")) {
@@ -65,7 +68,8 @@ bot.on("message", message => {
                         }, function (error, response, body) {
                             var result = JSON.parse(body);
                             var responseTime = new Date() - start;
-                            console.log(`[${chalk.bgRed('INFO')}] - ${result.message} \nFinished in: (${chalk.cyan(responseTime / 1000) + 's'})\n`);
+                            console.log(`[${chalk.bgRed('INFO')}] - ${result.message} \nFinished in: (${chalk.cyan(responseTime / 1000) + 's'})\n`)
+
 
                             notifier.notify({
                                 title: 'NatSniper',
